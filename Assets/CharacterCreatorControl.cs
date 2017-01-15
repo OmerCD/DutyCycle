f9b632574ca1664db2dbe9bc60b3f3f7ed7e9119
OmerCD <omercd@hotmail.com.tr>
2017-01-15 21:06:18 +0300

    Skill Point Assesment
    - Unity sliders are broken. They can't detect fast changes. Need to use
    alternatives instead of sliders.

--- Assets/CharacterCreatorControl.cs
+++ Assets/CharacterCreatorControl.cs
@@ -8,11 +8,38 @@ public class CharacterCreatorControl : MonoBehaviour
     [SerializeField]
     private GameObject skillField;
     private LanguageManager languageManager;
-    [SerializeField] private Text name;
+    [SerializeField]
+    private Text name, skillPointsText;
+    public static readonly int MaxSkillPoints = 50;
+    delegate void SkillPointShow(int points);
+
+    private static SkillPointShow showSkillPLeft;
+    private static int skillPointsLeft;
+    public static int SkillPointsLeft
+    {
+        get { return skillPointsLeft; }
+        set
+        {
+            showSkillPLeft(value);
+            skillPointsLeft = value;
+        } 
+    }
+
+    void ShowLeftSkillPoints(int skillPoints)
+    {
+        skillPointsText.text = languageManager.GetString("SkillPoints") + " : " + skillPoints;
+    }
+
+    void Awake()
+    {
+        languageManager = LanguageManager.Load();
+    }
 	// Use this for initialization
     void Start()
     {
-        languageManager = LanguageManager.Load();
+        showSkillPLeft = ShowLeftSkillPoints;
+        SkillPointsLeft = MaxSkillPoints;
+        
         name.text = languageManager.GetString("Name");
         ArrangeSkills();
 
---
+++ Assets/SkillPointSliderManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillPointSliderManager : MonoBehaviour {
+    UnityEngine.UI.Slider slider;
+    UnityEngine.UI.Text valueText;
+    int valueBefore;
+    // Use this for initialization
+    void Start () {
+        valueText = transform.parent.GetChild(2).GetComponent<UnityEngine.UI.Text>();
+        slider = GetComponent<UnityEngine.UI.Slider>();
+        slider.maxValue = CharacterCreatorControl.MaxSkillPoints;
+        valueBefore = (int)slider.value;
+	}
+    public void OnValueChanged()
+    {
+        if (valueBefore>slider.value)
+        {
+            CharacterCreatorControl.SkillPointsLeft+=valueBefore-(int)slider.value;
+        }
+        else
+        {
+            if (CharacterCreatorControl.SkillPointsLeft <= 0)
+            {
+                slider.value = valueBefore- CharacterCreatorControl.SkillPointsLeft;
+                CharacterCreatorControl.SkillPointsLeft = 0;
+            }
+            else {
+                CharacterCreatorControl.SkillPointsLeft-=(int)slider.value-valueBefore;
+            }
+        }
+        valueText.text = slider.value.ToString();
+        valueBefore = (int)slider.value;
+    }
+    void Update()
+    {
+        //if (CharacterCreatorControl.SkillPointsLeft <= 0)
+        //{
+        //    CharacterCreatorControl.SkillPointsLeft = 0;
+        //    slider.value = valueBefore;
+        //}
+    }
+}
